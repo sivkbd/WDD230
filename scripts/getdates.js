ff3f2d59fe62ae59739c7180b2771869cc3573af
sivkbd <siviwe.kibido@gmail.com>
2024-01-29 22:27:53 +0200

    updated homepage

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,11 @@
-const currentYear = new Date().getFullYear();
-document.write("© " + currentYear);
 
-document.getElementById("lastModified") = new Date(document.lastModified);
+let year = new Date().getFullYear();
+
+let copyright = `© ${year}`;
+
+
+let lastModified = `Last modified: ${document.lastModified}`;
+
+
+document.querySelector("footer > p:first-of-type").innerHTML = copyright;
+document.querySelector("#lastModified").innerHTML = lastModified;
